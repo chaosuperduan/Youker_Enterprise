2b496cdf2795fb12b0043901db0fe1c1aabc66fd
apple <apple@appledeMacBook-Pro.local>
2018-09-06 02:03:05 +0800

    😆
    南

---
+++ Youker_Enterprise/Classes/staffManage/Model/cpyInfo.swift
@@ -0,0 +1,54 @@
+//
+//  cpyInfo.swift
+//  Youker_Enterprise
+//
+//  Created by apple on 2018/9/6.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+class cpyInfo: NSObject {
+    var company_Address:String?
+    var company_Name:String?
+    var company_Phone:String?
+    var law_person_Name:String?
+    var reg_Address:String?
+    var usccode:String?
+    var userCount:String?
+    class func getDic(mode:cpyInfo)->NSDictionary{
+        var dic:[String:AnyObject] = [String:AnyObject]()
+        dic["company_Address"] = mode.company_Address as AnyObject
+        dic["company_Name"] = mode.company_Name as AnyObject
+        dic["company_Phone"] = mode.company_Phone as AnyObject
+        dic["law_person_Name"] = mode.law_person_Name as AnyObject
+        dic["reg_Address"] = mode.reg_Address as AnyObject
+        dic["usccode"] = mode.usccode as AnyObject
+        dic["userCount"] = mode.userCount as AnyObject
+        
+        
+    
+       return dic as NSDictionary
+    }
+    
+}
+
+class districts:NSObject{
+   var regCity:String?
+    var cpyDistrict:String?
+    var cpyCity:String?
+    var egDistrict:String?
+    
+    class func getDic(mode:districts)->NSDictionary{
+        var dic:[String:AnyObject] = [String:AnyObject]()
+        dic["regCity"] = mode.regCity as AnyObject
+        dic["cpyDistrict"] = mode.cpyDistrict as AnyObject
+        dic["cpyCity"] = mode.cpyCity as AnyObject
+        dic["egDistrict"] = mode.egDistrict as AnyObject
+        
+        return dic as NSDictionary
+        
+    }
+    
+    
+}
